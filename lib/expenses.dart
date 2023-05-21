1aca5aa6027eaea5e5a68559540e5c9c6f204adb
Simon Pierre Ndizihiwe <ndizihiwesimon@gmail.com>
2023-05-21 11:08:13 +0200

    working on expenses main screen

---
+++ lib/expenses.dart
@@ -0,0 +1,8 @@
+import 'package:flutter/material.dart';
+
+class Expenses extends StatefulWidget {
+  @override
+  State<Expenses> createState() => _ExpensesState();
+}
+
+class _ExpensesState extends State<Expenses> {}
